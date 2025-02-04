e988ddc340bb387b7e64a00f07e8b5e3061f9764
Marc Carroll <marc@deveire.com>
2025-02-04 22:15:10 +0000

    Update RelativeDate.vue

--- src/components/RelativeDate.vue
+++ src/components/RelativeDate.vue
@@ -3,12 +3,22 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
+import { ref, onMounted, onBeforeUnmount } from 'vue'
 import { getRelativeTime } from '@/util/getRelativeTime'
 
-const props = defineProps<{
-  date: Date
-}>()
+const props = defineProps<{ date: Date }>()
 
-const relativeTime = computed(() => getRelativeTime(props.date))
+const relativeTime = ref(getRelativeTime(props.date))
+
+let intervalId: ReturnType<typeof setInterval> | null = null
+
+onMounted(() => {
+  intervalId = setInterval(() => {
+    relativeTime.value = getRelativeTime(props.date)
+  }, 60 * 1000) // Update every 60 seconds
+})
+
+onBeforeUnmount(() => {
+  if (intervalId) clearInterval(intervalId)
+})
 </script>
